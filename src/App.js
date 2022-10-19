fdbbaacff0db2655110b6f103d68b66d3f1eb048
Anjum <88701810+Anjum7866@users.noreply.github.com>
2022-10-19 00:20:26 -0700

    deploying project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
-import Aiops from './component/Aiops';
 import Alira from './component/Alira';
-import Demo from './component/Demo';
 import Header from './component/Header';
-import Interview from './component/Interview';
 
 function App() {
   return (
